4062c61002139541d602eeeed7d7ab36dc2a4fe3
fsloth5 <gcelisramirez@student.cccd.edu>
2026-09-17 16:07:39 -0700

    Add platform detection

--- src/core/src/megan.cpp
+++ src/core/src/megan.cpp
@@ -3,4 +3,11 @@
 
 namespace megan {
 void encrypt() { std::cout << "Encrypting...\n"; }
+bool host_uses_crlf() {
+  return (platform_name_v == "DOS" || platform_name_v == "OS2" ||
+          platform_name_v == "Windows" || platform_name_v == "WindowsCE" ||
+          platform_name_v == "WindowsPhone" ||
+          platform_name_v == "WindowsStore");
+}
+
 } // namespace megan
